412252c4eb5aa345b5c30269dd37031d2154ad20
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-25 14:16:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArr, eventName){
+    let cards = []
+    for(let i = 0; i < namesArr.length; i ++){
+        cards.push("Thank you, " + namesArr[i] + ", for the wonderful surprise gift!")
+    }
+    return cards
+}
+
+function countdown(countdown){
+    while(countdown > 0){
+        console.log(countdown)
+        countdown -= 1
+    }
+    console.log(countdown)
+}
